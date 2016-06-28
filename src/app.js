bbc297f88ba6ba69b3a57c731a74bee907d549e7
Joseph Daniel Chapdelaine <orvist@gmail.com>
2016-06-28 15:27:09 -0500

    successful browserify test

---
+++ src/app.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let MathModule = require("./math");
+
+console.log("MathModule", MathModule);
+console.log("adding", MathModule.adding(3,5));
+console.log("subtracting", MathModule.subtract(10,3));
+console.log("multipling", MathModule.multiply(3,5));
+console.log("dividing", MathModule.divide(10,5));
